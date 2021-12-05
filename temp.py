bafeef36b0db4c33dca0a07e9105a6471c413669
AyushGupta2301 <ayush2301ak@gmail.com>
2021-12-05 14:48:42 +0530

    temp

--- temp.py
+++ temp.py
@@ -1,3 +1,5 @@
-a = {}
-a["tt"] = 5
-print(a)
+import requests
+
+message ="Hello \n Sir" 
+paramperson = {'chat_id':'1158109698','text':message,'parse_mode':'HTML'}
+resp = requests.post('https://api.telegram.org/bot2133097358:AAFLewQeNHQysJS6sOIJ0L1ZMRuSX-rNTc8/sendMessage',params=paramperson)
